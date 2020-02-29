56da807fdf18f599bc360f53fb567f2eefc46483
XiaoleZ <xiaole2@ualberta.ca>
2020-02-28 22:32:50 -0700

    rename

--- publishconf.py
+++ publishconf.py
@@ -13,6 +13,7 @@ from pelicanconf import *
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://XiaoleZ.github.io'
 RELATIVE_URLS = False
+OUTPUT_PATH = 'cmput404-lab5'
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 #CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
