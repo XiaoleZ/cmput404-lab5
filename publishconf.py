a6cd613fe8dc9c13b45fc635fd549ad1db9015b0
XiaoleZ <xiaole2@ualberta.ca>
2020-02-28 22:37:45 -0700

    rename

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://XiaoleZ.github.io'
+SITEURL = 'https://XiaoleZ.github.io/cmput404-lab5/'
 RELATIVE_URLS = False
 OUTPUT_PATH = 'cmput404-lab5'
 
